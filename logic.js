46e30581c9fffa8c1d00208cfa3a257f47eea18d
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-02-13 17:00:26 +0200

    deleted some lines

--- logic.js
+++ logic.js
@@ -1,28 +1,9 @@
 
 const Tweeter = function () {
 
-    const _posts = [
-        {
-            text: "First post!",
-            id: "p1",
-            comments: [
-                { id: "c1", text: "First comment on first post!" },
-                { id: "c2", text: "Second comment on first post!!" },
-                { id: "c3", text: "Third comment on first post!!!" }
-            ]
-        },
-        {
-            text: "Aw man, I wanted to be first",
-            id: "p2",
-            comments: [
-                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-                { id: "c5", text: "Yeah, believe in yourself!" },
-                { id: "c6", text: "Haha second place what a joke." }
-            ]
-        }
-    ]
-    let postIdCounter = 1
-    let commentIdCounter = 1
+    const _posts = []
+    let _postIdCounter = 1
+    let _commentIdCounter = 1
 
     const getPosts = function() {
         return _posts
@@ -30,7 +11,7 @@ const Tweeter = function () {
 
     const addPost = function (text) {
         _posts.push({
-            id: "p" + postIdCounter++,
+            id: "p" + _postIdCounter++,
             text,
             comments: []
         })
@@ -43,7 +24,7 @@ const Tweeter = function () {
     const addComment = function (postID, text) {
         const postIndex = _posts.findIndex(post => post.id === postID)
         _posts[postIndex].comments.push({
-            id: "c" + commentIdCounter++,
+            id: "c" + _commentIdCounter++,
             text
         })
     }
--- render.js
+++ render.js
@@ -4,7 +4,9 @@ const Renderer = function () {
     const createCommentsElement = function (comments, postID) {
         let commentsList = $(`<ul class="${postID}-comments"></ul>`)
         for (let comment of comments) {
-            commentsList.append(`<li data-id=${comment.id}><button class="delete-comment">X</button>${comment.text}</li>`)
+            commentsList.append(`<li data-id=${comment.id}>
+                                    <button class="delete-comment">X</button>${comment.text}
+                                </li>`)
         }
         return commentsList
     }
@@ -13,7 +15,9 @@ const Renderer = function () {
         $("#posts").empty()
         for (let post of posts) {
             $("#posts").append(`<div data-id=${post.id}>${post.text}</div>`)
-            $(`[data-id="${post.id}"]`).append(`<br><input type="text" placeholder="add comment"/><button class="comment-button">Add Comment</button>`)
+            $(`[data-id="${post.id}"]`).append(`<br>
+                                                <input type="text" placeholder="add comment"/>
+                                                <button class="comment-button">Add Comment</button>`)
             let commentsElement = createCommentsElement(post.comments, post.id)
             $(`[data-id="${post.id}"]`).append(commentsElement)
             $(`[data-id="${post.id}"]`).append(`<button class="delete-button">Delete Post</button>`)
