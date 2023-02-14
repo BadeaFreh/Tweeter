1aed1751f6c330d5d38a08c1d5c2ff8a3658cdb1
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-02-14 10:48:26 +0200

    deleted helper function from main.js

--- main.js
+++ main.js
@@ -4,33 +4,30 @@ const renderer = Renderer()
 
 renderer.renderPosts(tweeter.getPosts())
 
-const getPostsAndRender = function () {
-    let posts = tweeter.getPosts()
-    renderer.renderPosts(posts)
-}
-
 $("#post").on("click", function () {
     let postText = $("input").val()
     tweeter.addPost(postText)
-    getPostsAndRender()
+    renderer.renderPosts(tweeter.getPosts())
+    $("#input").val("")
 })
 
 $("#posts").on("click", ".delete-button", function () {
     const postID = $(this).closest("div").data().id
     tweeter.removePost(postID)
-    getPostsAndRender()
+    renderer.renderPosts(tweeter.getPosts())
+
 })
 
 $("#posts").on("click", ".comment-button", function () {
     const postID = $(this).closest("div").data().id
     const commentText = $(this).closest("div").find("input").val()
     tweeter.addComment(postID, commentText)
-    getPostsAndRender()
+    renderer.renderPosts(tweeter.getPosts())
 })
 
 $("#posts").on("click", ".delete-comment", function () {
     const postID = $(this).closest("div").data().id
     const commentID = $(this).closest("li").data().id
     tweeter.removeComment(postID, commentID)
-    getPostsAndRender()
+    renderer.renderPosts(tweeter.getPosts())
 })
--- render.js
+++ render.js
@@ -15,9 +15,10 @@ const Renderer = function () {
         $("#posts").empty()
         for (let post of posts) {
             $("#posts").append(`<div data-id=${post.id}>${post.text}</div>`)
-            $(`[data-id="${post.id}"]`).append(`<br>
-                                                <input type="text" placeholder="add comment"/>
-                                                <button class="comment-button">Add Comment</button>`)
+            $(`[data-id="${post.id}"]`).append(`
+                                                <br><input type="text" placeholder="add comment"/>
+                                                <button class="comment-button">Add Comment</button>
+                                                `)
             let commentsElement = createCommentsElement(post.comments, post.id)
             $(`[data-id="${post.id}"]`).append(commentsElement)
             $(`[data-id="${post.id}"]`).append(`<button class="delete-button">Delete Post</button>`)
