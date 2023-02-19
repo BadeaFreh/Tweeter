b5e726ab818344b4081e3793c80eb7860993abbd
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-02-20 00:37:22 +0200

    changed styling

--- render.js
+++ render.js
@@ -1,3 +1,4 @@
+{/* <button type="button" class="btn btn-primary">Primary</button> */}
 
 const Renderer = function () {
 
@@ -14,14 +15,15 @@ const Renderer = function () {
     const renderPosts = function (posts) {
         $("#posts").empty()
         for (let post of posts) {
-            $("#posts").append(`<div data-id=${post.id}>${post.text}</div>`)
+            $("#posts").append(`<div class="post" data-id=${post.id}><p>${post.text}</p></div>`)
             $(`[data-id="${post.id}"]`).append(`
-                                                <br><input type="text" placeholder="add comment"/>
-                                                <button class="comment-button">Add Comment</button>
+                                                
+                                                <input type="text" placeholder="add comment"/>
+                                                <button class="comment-button btn btn-primary btn-sm">Comment</button>
                                                 `)
             let commentsElement = createCommentsElement(post.comments, post.id)
             $(`[data-id="${post.id}"]`).append(commentsElement)
-            $(`[data-id="${post.id}"]`).append(`<button class="delete-button">Delete Post</button>`)
+            $(`[data-id="${post.id}"]`).append(`<button class="delete-button btn btn-danger btn-sm">Delete</button>`)
         }
     }
 
